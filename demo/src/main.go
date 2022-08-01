92266e2795f66f04d0c77e544ea2f9883559e82b
gjl <jiale.guo1012@gmail.com>
2022-08-01 20:56:11 +0800

    update go demo

--- demo/src/main.go
+++ demo/src/main.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-	"bytes"
+	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 
-	"github.com/bitly/go-simplejson"
-	"github.com/ghodss/yaml"
-	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
-	"github.com/hyperledger/fabric-sdk-go/pkg/client/msp"
-	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/context"
-	mspapi "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
-	pmsp "github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
-	contextImpl "github.com/hyperledger/fabric-sdk-go/pkg/context"
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
-	"github.com/hyperledger/fabric-sdk-go/pkg/fab"
+	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+
+	"io"
+	"bytes"
+	"strings"
+	"encoding/json"
+  "github.com/bitly/go-simplejson"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
-	"github.com/pkg/errors"
+	"github.com/ghodss/yaml"
 	"github.com/spf13/viper"
+
+
 )
+
 
 // Global variable
 var (
 	sdk         *fabsdk.FabricSDK
-	configFile  = "/Users/lisagjl/code/bcs/gosdkdemo/config/gosdkdemo-channel-sdk-config.yaml"
+	configFile  = "/home/jiale001/bcs/data_synthesis/demo/config/bcs-test-channel-sdk-config.json"
 	org         = "4f08db41ded98093a7266580a4a2ae3ce62ce74a"
 	sdkfile     *simplejson.Json
 	chaincodeID string
@@ -35,26 +34,117 @@ var (
 	privateKey  string
 )
 
-const emptyString = ""
-
 func main() {
+	os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
+	wallet, err := gateway.NewFileSystemWallet("wallet")
+	if err != nil {
+		fmt.Printf("Failed to create wallet: %s\n", err)
+		os.Exit(1)
+	}
+
+	if !wallet.Exists("admin") {
+		err = populateWallet(wallet)
+		if err != nil {
+			fmt.Printf("Failed to populate wallet contents: %s\n", err)
+			os.Exit(1)
+		}
+	}
 	// load config file to config
 	loadConfig()
 	// initialize sdk
 	initializeSdk()
-	// insert data 
-	// ReadAsset("ReadAsset")
-	addEventwithAsset("AddCTEwithAsset")
-	// ReadAsset("ReadAsset")
+
+
+
+	gw, err := gateway.Connect(
+		gateway.WithSDK(sdk),
+		gateway.WithIdentity(wallet, "admin"),
+	)
+	if err != nil {
+		fmt.Printf("Failed to connect to gateway: %s\n", err)
+		os.Exit(1)
+	}
+	defer gw.Close()
+
+	network, err := gw.GetNetwork("channel")
+	if err != nil {
+		fmt.Printf("Failed to get network: %s\n", err)
+		os.Exit(1)
+	}
+
+	contract := network.GetContract("fisherysc")
+
+	// query asset
+	readAsset(contract)
+	// submit data
+	addCTEwithAsset(contract)
 
 }
 
-// loadConfig load the config file to initialize some Global variable
+// Submit a transaction synchronously, blocking until it has been committed to the ledger.
+func addCTEwithAsset(contract *gateway.Contract) {
+	fmt.Printf("Submit Transaction: CreateAsset, creates new asset with ID, Color, Size, Owner and AppraisedValue arguments \n")
+
+	_, err := contract.SubmitTransaction("addCTEwithAsset", "asset1", "1", "62567598498626", "62567598498626", "3604604142873", "107HAXI", "2022-Jul-23T08:43:08 +0000", "10")
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	fmt.Printf("*** Transaction committed successfully\n")
+}
+
+// Evaluate a transaction by assetID to query ledger state.
+func readAsset(contract *gateway.Contract) {
+	evaluateResult, err := contract.EvaluateTransaction("ReadAsset", "asset1")
+	if err != nil {
+		fmt.Printf("Failed to evaluate transaction: %s\n", err)
+		os.Exit(1)
+	}
+	result := formatJSON(evaluateResult)
+
+	fmt.Printf("*** Result:%s\n", result)
+}
+
+func populateWallet(wallet *gateway.Wallet) error {
+	credPath := "/home/jiale001/bcs/data_synthesis/demo/config/4f08db41ded98093a7266580a4a2ae3ce62ce74a.peer/msp"
+
+	certPath := filepath.Join(credPath, "signcerts", "cert.pem")
+	// read the certificate pem
+	cert, err := ioutil.ReadFile(filepath.Clean(certPath))
+	if err != nil {
+		return err
+	}
+
+	keyDir := filepath.Join(credPath, "keystore")
+	// there's a single file in this dir containing the private key
+	files, err := ioutil.ReadDir(keyDir)
+	if err != nil {
+		return err
+	}
+	if len(files) != 1 {
+		return errors.New("keystore folder should have contain one file")
+	}
+	keyPath := filepath.Join(keyDir, files[0].Name())
+	key, err := ioutil.ReadFile(filepath.Clean(keyPath))
+	if err != nil {
+		return err
+	}
+
+	identity := gateway.NewX509Identity("4f08db41ded98093a7266580a4a2ae3ce62ce74aMSP", string(cert), string(key))
+
+	err = wallet.Put("admin", identity)
+	if err != nil {
+		return err
+	}
+	// fmt.Println("wallet done")
+	return nil
+}
+
 func loadConfig() {
 	data, _ := ReadFile(configFile)
 	data, _ = yaml.YAMLToJSON(data)
 	sdkfile, _ = simplejson.NewJson(data)
-	fmt.Println(sdkfile)
+	//fmt.Println(sdkfile)
 	channelID = GetDefaultChannel()
 	chaincodeID = GetDefaultChaincodeId()
 
@@ -151,72 +241,6 @@ func GetDefaultChannel() string {
 	return ""
 }
 
-//GetCryptoPath get msp directory from sdk config file's path
-func GetCryptoPath(orgId string) string {
-	cryptoPath := sdkfile.Get("organizations").Get(orgId).Get("cryptoPath").MustString()
-	return cryptoPath
-}
-
-//GetTlsCryptoKeyPath get tlsCryptoKeyPath from sdk config file's path with orgId
-func GetTlsCryptoKeyPath(orgId string) string {
-	tlsCryptoKeyPath := sdkfile.Get("organizations").Get(orgId).Get("tlsCryptoKeyPath").MustString()
-	return tlsCryptoKeyPath
-}
-
-//GetSigncertsBytes can get Signcerts from sdk config file's path
-//cryptoPath is get from function GetCryptoPath
-func GetSigncertsBytes(orgId string) ([]byte, error) {
-	cryptoPath := GetCryptoPath(orgId)
-	signcertsPathdir := filepath.Join(cryptoPath, "signcerts")
-	files, _ := ioutil.ReadDir(signcertsPathdir)
-	if len(files) != 1 {
-		return nil, errors.Errorf("file count invalid in the directory [%s]", signcertsPathdir)
-	}
-
-	f, err := ioutil.ReadFile(filepath.Join(signcertsPathdir, files[0].Name()))
-	if err != nil {
-		return nil, errors.Errorf("read signcerts from [%s] fail", files[0].Name())
-	} else if f == nil {
-		return nil, errors.Errorf("result of read signcerts file [%s] is null", files[0].Name())
-	}
-
-	return f, nil
-}
-
-//GetPrivateKeyBytes can get privateKey from sdk config file's path
-//cryptoPath is get from function GetCryptoPath
-func GetPrivateKeyBytes(orgId string) ([]byte, error) {
-	cryptoPath := GetCryptoPath(orgId)
-	keystorePathdir := filepath.Join(cryptoPath, "keystore")
-	files, _ := ioutil.ReadDir(keystorePathdir)
-	if len(files) != 1 {
-		return nil, errors.Errorf("file count invalid in the directory [%s]", keystorePathdir)
-	}
-
-	f, err := ioutil.ReadFile(filepath.Join(keystorePathdir, files[0].Name()))
-	if err != nil {
-		return nil, errors.Errorf("read signcerts from [%s] fail", files[0].Name())
-	} else if f == nil {
-		return nil, errors.Errorf("result of read keystore file [%s] is null", files[0].Name())
-	}
-
-	return f, nil
-}
-
-//GetTlsCryptoKey can get tlsCryptoKey content from tlsCryptoKeyPath
-//tlsCryptoKeyPath is get from function GetTlsCryptoKeyPath
-func GetTlsCryptoKey(orgId string) (string, error) {
-	tlsCryptoKeyPath := GetTlsCryptoKeyPath(orgId)
-	f, err := ioutil.ReadFile(tlsCryptoKeyPath)
-	if err != nil {
-		return "", errors.Errorf("read tlsCryptoKey from [%s] fail", tlsCryptoKeyPath)
-	} else if f == nil {
-		return "", errors.Errorf("result of read tlsCryptoKey file [%s] is null", tlsCryptoKeyPath)
-	}
-
-	return string(f), nil
-}
-
 // getOptsToInitializeSDK is a function to initialize SDK
 func getOptsToInitializeSDK(configFile string) ([]fabsdk.Option, error) {
 	var opts []fabsdk.Option
@@ -239,172 +263,10 @@ func getOptsToInitializeSDK(configFile string) ([]fabsdk.Option, error) {
 	return opts, nil
 }
 
-// ChannelClient creates a new channel client
-func ChannelClient(channelID string, user mspapi.SigningIdentity) (*channel.Client, error) {
-	session := sdk.Context(fabsdk.WithIdentity(user))
-	contextImpl.NewChannel(session, channelID)
-	channelProvider := func() (context.Channel, error) {
-		return contextImpl.NewChannel(session, channelID)
+func formatJSON(data []byte) string {
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, data, " ", ""); err != nil {
+		panic(fmt.Errorf("failed to parse JSON: %w", err))
 	}
-	return channel.New(channelProvider)
-}
-
-// UserIdentityWithOrgAndName Identify users through org and Name,
-// which can pass cert and private key with external variables
-// if cert or pvtKey is empty, it will read from file path in sdk config file
-func UserIdentityWithOrgAndName(orgID string, userName string, cert []byte, pvtKey string) (mspapi.SigningIdentity, error) {
-	if userName == "" {
-		return nil, errors.Errorf("No username specified")
-	}
-
-	mspClient, err := msp.New(sdk.Context(), msp.WithOrg(orgID))
-	if err != nil {
-		return nil, errors.Errorf("Error creating MSP client: %s", err)
-	}
-
-	if pvtKey == emptyString {
-		user, err := mspClient.GetSigningIdentity(userName)
-		if err != nil {
-			return nil, errors.Errorf("GetSigningIdentity returned error: %v", err)
-		}
-		return user, nil
-
-	} else {
-		if cert == nil {
-			if cert, err = GetSigncertsBytes(orgID); err != nil {
-				return nil, err
-			}
-		}
-
-		// pvtKey must be decrypted when it is passed in function CreateSigningIdentity.
-		user, err := mspClient.CreateSigningIdentity(pmsp.WithCert(cert), pmsp.WithPrivateKey([]byte(pvtKey)))
-		if err != nil {
-			return nil, errors.Errorf("CreateSigningIdentity returned error: %v", err)
-		}
-		return user, nil
-	}
-	return nil, nil
-}
-
-
-func addEventwithAsset(fName string) (channel.Response, error) {
-	//We can get private key(encrypted or not) using function GetPrivateKeyBytes
-	//We recommend that you set the private key to be decrypted here using function SetPrivateKey。
-	//for example:
-	//encryptedbytekey,_:= GetPrivateKeyBytes(org)
-	//decrypt the encryptedkey
-	//SetPrivateKey(decryptedKey)
-	//if cert or privateKey is empty, anyone of them will read from file path in sdk config file
-	user, err := UserIdentityWithOrgAndName(org, "Admin", nil, privateKey)
-	if err != nil {
-		panic(err)
-	}
-
-	//Generating the Channel Context
-	chClient, err := ChannelClient(channelID, user)
-	if err != nil {
-		panic(err)
-	}
-
-	//The client need to send a requset to the channel Chaincode Name,Function Name, Parameter
-	args := [][]byte{
-		[]byte("asset1"),
-		[]byte("1"),
-		[]byte("62567598498627"),
-		[]byte("62567598498627"),
-		[]byte("3604604142873"),
-		[]byte("107HAXI"),
-		[]byte("2022-Jul-24T08:43:08 +0000"),
-		[]byte("10"),
-	}
-	response, err := chClient.Execute(
-		channel.Request{
-			ChaincodeID: chaincodeID,
-			Fcn:         fName,
-			Args:        args,      
-		})
-	if err != nil {
-		fmt.Println("insert faild", err)
-		panic(err)
-	}
-	fmt.Printf("store new data success\n")
-	fmt.Printf("response: ", response, "\n")
-	return response, nil
-}
-
-// Query Function
-func ReadAsset(fName string) (channel.Response, error) {
-	//We can get private key(encrypted or not) using function GetPrivateKeyBytes
-	//We recommend that you set the private key to be decrypted here using function SetPrivateKey。
-	//for example:
-	//encryptedkey,_:= GetPrivateKeyBytes(org)
-	//decrypt the encryptedkey
-	//SetPrivateKey(decryptedKey)
-	//if cert or privateKey is empty, anyone of them will read from file path in sdk config file
-	user, err := UserIdentityWithOrgAndName(org, "Admin", nil, privateKey)
-	if err != nil {
-		panic(err)
-	}
-
-	chClient, err := ChannelClient(channelID, user)
-	if err != nil {
-		panic(err)
-	}
-
-	// The client need to send a requset to the channel Chaincode Name,Function Name, Parameter
-	args := [][]byte{
-			[]byte("asset1")}
-	queryRes, err := chClient.Query(channel.Request{
-		ChaincodeID: chaincodeID,
-		Fcn:         fName,
-		Args:        args,
-	})
-	fmt.Printf("query key <%s> value is %s\n", string(args[0]), string(queryRes.Payload))
-	return queryRes, err
-}
-
-
-
-//SetPrivateKey update the privateKey used in ChannelClient
-func SetPrivateKey(key string) {
-	privateKey = key
-}
-
-//ClearPrivateKey set privateKey empty
-func ClearPrivateKey() {
-	privateKey = ""
-}
-
-//SetClientTlsKey update the tls key in fabric-sdk
-func SetClientTlsKey(tlsKey string) {
-	vc := viper.New()
-	vc.SetConfigFile(configFile)
-	err := vc.ReadInConfig()
-	if err != nil {
-		panic(fmt.Sprintf("Failed to read configFile: %s", configFile))
-	}
-
-	orgID := vc.GetString("client.originalOrganization")
-	if orgID == "" {
-		orgID = vc.GetString("client.organization")
-	}
-
-	//SetTlsClientKey can be used to update the tlskey in fabric-sdk
-	fab.SetTlsClientKey(orgID, tlsKey)
-}
-
-//ClearClientTlsKey reset the tls key in fabric-sdk with tls key file specified in config
-func ClearClientTlsKey() {
-	vc := viper.New()
-	vc.SetConfigFile(configFile)
-	err := vc.ReadInConfig()
-	if err != nil {
-		panic(fmt.Sprintf("Failed to read configFile: %s", configFile))
-	}
-
-	orgID := vc.GetString("client.originalOrganization")
-	if orgID == "" {
-		orgID = vc.GetString("client.organization")
-	}
-	fab.ResetTlsClientKeyWithOrgID(orgID)
+	return prettyJSON.String()
 }
